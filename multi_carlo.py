ffa0d1ca2cc0306661ab74f551f67c6207d46a29
vipandyc <82727137+vipandyc@users.noreply.github.com>
2022-10-23 17:41:53 +0800

    Update multi_carlo.py

--- multi_carlo.py
+++ multi_carlo.py
@@ -60,7 +60,7 @@ if __name__=='__main__':
 
     unit=1000
     maxtime_iter=100000
-    walkers=50 # number of walkers, can change
+    walkers=100 # number of walkers, can change
     num_cores = int(mp.cpu_count())
     pool = mp.Pool(num_cores)
 
